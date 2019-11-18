7ff6bd2aa7969b59a5770006094bfefb06060f0a
Angrey Shoful <shoful.a@gmail.com>
2019-11-18 11:57:51 +0200

    Updates

---
+++ precache-manifest.96e1344d3e305c64996c6a535978864e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496b8d416aa64c780ef70fd4196670c2",
+    "url": "/test-voting-app/index.html"
+  },
+  {
+    "revision": "93282b9ab2134fce1e18",
+    "url": "/test-voting-app/static/css/main.9d96ce17.chunk.css"
+  },
+  {
+    "revision": "37c18edd02c9270e8704",
+    "url": "/test-voting-app/static/js/2.a1868ea8.chunk.js"
+  },
+  {
+    "revision": "93282b9ab2134fce1e18",
+    "url": "/test-voting-app/static/js/main.ef124e9e.chunk.js"
+  },
+  {
+    "revision": "bbbb6c314ee67848b27c",
+    "url": "/test-voting-app/static/js/runtime-main.747bc12d.js"
+  },
+  {
+    "revision": "507a0e33a4e083f830833e401e959b93",
+    "url": "/test-voting-app/static/media/one.507a0e33.png"
+  },
+  {
+    "revision": "9c6efd85f59f4af063a380864da8c3f9",
+    "url": "/test-voting-app/static/media/two.9c6efd85.png"
+  }
+]);
